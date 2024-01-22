629fcfa3699cb8212b84bff3c1de805de4044b61
Fahim Al Jadid <b170606023@pharm.jnu.ac.bd>
2024-01-22 07:46:08 +0600

    added root

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Root from "./routes/root";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.jsx";
@@ -6,7 +7,7 @@ import "./index.css";
 
 const router = createBrowserRouter({
   path: "/",
-  element: <div>hello</div>,
+  element: <Root />,
 });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
